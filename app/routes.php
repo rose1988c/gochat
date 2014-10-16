a1a0d1d67e3ff2845a0377a4498f22d2c1020f96
rose1988c <rose1988.c@gmail.com>
2014-10-16 22:06:39 +0800

    update cron qq

--- app/routes.php
+++ app/routes.php
@@ -1,30 +1,16 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
+Route::get('/', function ()
 {
-	return View::make('hello');
+    return View::make('hello');
 });
 
-<<<<<<< HEAD
-Route::group(array('prefix' => 'cron'), function(){
-
-    Route::get('/', function()
+Route::group(array (
+    'prefix' => 'cron' 
+), function ()
+{
+    Route::get('/', function ()
     {
-	return View::make('hello');
+        return View::make('hello');
     });
-
-    });
-=======
+});
 Route::get('/chat', 'ChatController@index');
->>>>>>> 51ca7a3172409ce74f5cbac4de46768573c3104c
